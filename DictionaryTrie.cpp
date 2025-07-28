8708491f4059f394df563552d3d8159e74c7d034
om bhutkar <ombhutkar9@gmail.com>
2025-07-28 08:18:46 +0530

    minor changes

--- DictionaryTrie.cpp
+++ DictionaryTrie.cpp
@@ -12,10 +12,7 @@ DictionaryTrie::DictionaryTrie(){
     count = 0;                // and count as 0
 }
 
-/* Insert a word with its frequency into the dictionary.
- * Return true if the word was inserted, and false if it
- * was not (i.e. it was already in the dictionary or it was
- * invalid (empty string) */
+
 bool DictionaryTrie::insert(std::string word, unsigned int freq)
 {
     int wordSize = word.size(); // get the length of the word
@@ -132,16 +129,7 @@ bool DictionaryTrie::find(std::string word) const
     }
 }
 
-/* Return up to num_completions of the most frequent completions
- * of the prefix, such that the completions are words in the dictionary.
- * These completions should be listed from most frequent to least.
- * If there are fewer than num_completions legal completions, this
- * function returns a vector with as many completions as possible.
- * If no completions exist, then the function returns a vector of size 0.
- * The prefix itself might be included in the returned words if the prefix
- * is a word (and is among the num_completions most frequent completions
- * of the prefix)
- */
+
 std::vector<std::string> DictionaryTrie::predictCompletions(std::string prefix, 
         unsigned int num_completions) {
 
